23b1568c82a591d7a23c3408f27eca9467cefcea
usovigorok <usovigorok@gmail.com>
2018-03-08 17:28:49 +0100

    Part done

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
-// import classes from './Layout.css';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = ( props ) => (
     <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <Toolbar />
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const logo = (props) => (
+    <div>
+        <img src={} />
+    </div>
+);
+
+export default logo; 
